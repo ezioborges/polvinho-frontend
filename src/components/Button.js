4bf0a49b87b5164c00362b2002858bbd69cddb30
09345145931 <ezioborgesdev@gmail.com>
2025-04-19 17:16:05 -0300

    salvando progresso

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,6 +4,7 @@ const Button = (title) => {
     const buttonContent = newElement('button');
     buttonContent.textContent = title
     buttonContent.classList.add('button-content')
+    buttonContent.classList.add('textMdBold')
     
     return buttonContent
 }
--- src/pages/Exam.js
+++ src/pages/Exam.js
@@ -2,6 +2,7 @@ import ExamBody from "../components/Exam/index.js";
 import PageTitle from "../components/PageTitle.js";
 import { quizzesData } from "../data/quizzesData.js";
 import newElement from "../utils/newElement.js"
+import textGenerator from "../utils/textGenerator.js";
 
 const Exam = () => {
     const hash = window.location.hash
@@ -11,16 +12,30 @@ const Exam = () => {
 
     const { name, discipline } = data.find((quiz) => quiz.id == hashId)
 
+    const bodyContent = newElement('div')
+    bodyContent.classList.add('exam-component')
+
     const examContent = newElement('div')
-    examContent.classList.add('exam-content')    
+    examContent.classList.add('exam-content') 
+    
+    const attemptsArea = newElement('div')
+    attemptsArea.classList.add('attempts-area')
+
+    const titleAttempts = textGenerator('textLG', 'Suas tentativas')
+    attemptsArea.style.color = 'var(--stone-700)'
     
     const title = PageTitle(name, discipline)
     const body = ExamBody()
 
+    attemptsArea.appendChild(titleAttempts)
+
     examContent.appendChild(title)
     examContent.appendChild(body)
 
-    return examContent;
+    bodyContent.appendChild(examContent)
+    bodyContent.appendChild(attemptsArea)
+
+    return bodyContent;
 }
 
 export default Exam;
