08dbe39b8789e5f916ebe4645c3c8f03e1eee122
09345145931 <ezioborgesdev@gmail.com>
2025-05-07 15:10:22 -0300

    feat: enhance QuizzBody layout and styling; restructure question and answer presentation

--- src/components/Quizz/QuizzBody.js
+++ src/components/Quizz/QuizzBody.js
@@ -7,22 +7,54 @@ const QuizzBody = (questions) => {
     bodyContent.classList.add('quizz-body')
     
     questions.forEach((question) => {
+        const questionContainer = newElement('div')
+        questionContainer.classList.add('question-container')
+
+        const questionArea = newElement('div')
+        questionArea.classList.add('question-area')
+
+        const answersArea = newElement('div')
+        answersArea.classList.add('answers-area')
+        // número da pergunta
+        const questionNumber = newElement('p')
+        questionNumber.textContent = `Pergunta: ${question.questionId}`
+        questionNumber.classList.add('question-number')
+        questionNumber.classList.add('textXL')
+        bodyContent.appendChild(questionNumber)
+
+
         // titulo de cada pergunta
         const questionTitle = newElement('p')
-        questionTitle.textContent = `AQUI EU AMONTO AS PERGUNTAS: ${question.question}`
+        questionTitle.textContent = `${question.question}`
+        questionTitle.classList.add('question-title')
+        questionTitle.classList.add('textMd')
         bodyContent.appendChild(questionTitle)
 
         // lista para as alternativas
-        const optionsList = newElement('ul')
+        const optionsList = newElement('ol')
+        optionsList.classList.add('option-list')
 
         // itera sobre as alternativas da pergunta
         question.alternatives.forEach((alternative) => {
             const listItem = newElement('li')
-            listItem.textContent = alternative
+            const text = newElement('p')
+            text.style.color = 'var(--stone-700)'
+            text.textContent = alternative
+            listItem.classList.add('list-item')
+            text.classList.add('textMd')
+            listItem.appendChild(text)
             optionsList.appendChild(listItem)
         })
 
-        bodyContent.appendChild(optionsList)
+        questionArea.appendChild(questionNumber)
+        questionArea.appendChild(questionTitle)
+
+        answersArea.appendChild(optionsList)
+
+        questionContainer.appendChild(questionArea)
+        questionContainer.appendChild(answersArea)
+
+        bodyContent.appendChild(questionContainer)
     })
 
     return bodyContent;
