59a966c67668a8dbf483779ff8af9b9d22633458
Ezio Borges <eziopborges@gmail.com>
2025-05-30 11:55:12 -0300

    inserir o title no component DashContent

--- src/components/Dashboard/DashListItems.js
+++ src/components/Dashboard/DashListItems.js
@@ -2,9 +2,14 @@ import newElement from "../../utils/newElement.js";
 import BodyWithoutContent from "../BodyWithoutContent.js";
 import { entities } from "../../data/adminEntities.js"
 import BarItem from "./BarItem.js";
+import textGenerator from "../../utils/textGenerator.js";
 
 const DashContent = () => {
     const content = newElement('div')
+
+    const title = textGenerator('title3', 'Entidades');
+
+    content.appendChild(title)
 
     const noContent = BodyWithoutContent('Nenhum conteúdo disponível');
 
