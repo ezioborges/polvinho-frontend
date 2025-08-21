be92a864c99bbcacbcef82ea8140e3faac3ad382
Ezio Borges <eziopborges@gmail.com>
2025-08-21 10:00:50 -0300

    fix: Update BASE_URL to point to production API endpoint

--- src/urls/index.js
+++ src/urls/index.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://localhost:2424';
-// 'https://little-octopus-api.onrender.com';
+export const BASE_URL = 'https://little-octopus-api.onrender.com';
+// 'http://localhost:2424';
 
 const urls = {
 	login: `${BASE_URL}/login`,
