75b6783c7b1b9f89dac45d5e08cd4f8efd86054d
Ezio Borges <eziopborges@gmail.com>
2025-08-21 21:35:30 -0300

    fix: Correct BASE_URL assignment for production environment

--- src/urls/index.js
+++ src/urls/index.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:2424';
-// export const BASE_URL = 'https://little-octopus-api.onrender.com';
+export const BASE_URL = 'https://little-octopus-api.onrender.com';
+// export const BASE_URL = 'http://localhost:2424';
